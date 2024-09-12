b179a91abb7c56885e5ad2d6cb102570633b37c0
imenegherbi00 <imenegherbi00@gmail.com>
2024-09-12 04:11:01 +0100

    git_actions

--- test_test.py
+++ test_test.py
@@ -1,17 +1,17 @@
 import re
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Page, expect
 
-def run(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=True, slow_mo=2000)  # Set headless mode to True
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
+def test_has_title(page: Page):
     page.goto("https://playwright.dev/")
-    page.get_by_role("link", name="Get started").click()
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
-    context.tracing.stop(path="traces/trace.zip")
-    context.close()
-    browser.close()
 
-with sync_playwright() as playwright:
-    run(playwright)
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
