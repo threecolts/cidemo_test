9b20a591d0ddb9369519f88af80685deb48de09e
imenegherbi00 <imenegherbi00@gmail.com>
2024-09-12 04:09:32 +0100

    git_actions

--- test_test.py
+++ test_test.py
@@ -1,19 +1,17 @@
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-
 def run(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False, slow_mo=2000)
+    browser = playwright.chromium.launch(headless=True, slow_mo=2000)  # Set headless mode to True
     context = browser.new_context()
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
     page.goto("https://playwright.dev/")
     page.get_by_role("link", name="Get started").click()
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
-    context.tracing.stop(path = "traces/trace.zip")
+    context.tracing.stop(path="traces/trace.zip")
     context.close()
     browser.close()
-
 
 with sync_playwright() as playwright:
     run(playwright)
